44aef05413fe37bc960aaf862cdf8649a79931b1
xpf <xpf@junquan.com.cn>
2018-10-25 11:42:51 +0800

    SpringBoot 整合 freemarker

---
+++ Code/trunk/SpringBootFreemarker/src/main/java/com/demo/controller/HelloController.java
@@ -0,0 +1,32 @@
+package com.demo.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value="/index")
+	public String index(Map<String, Object> map ){
+		map.put("name", "Joe");
+        map.put("sex", 1);    //sex:性别，1：男；0：女；  
+        
+        // 模拟数据
+        List<Map<String, Object>> friends = new ArrayList<Map<String, Object>>();
+        Map<String, Object> friend = new HashMap<String, Object>();
+        friend.put("name", "xbq");
+        friend.put("age", 22);
+        friends.add(friend);
+        friend = new HashMap<String, Object>();
+        friend.put("name", "July");
+        friend.put("age", 18);
+        friends.add(friend);
+        map.put("friends", friends);
+        return "freemarker";
+	}
+}
